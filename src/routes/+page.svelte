d2a31ba909a36920314fac8ce4f28ea618575ad5
Arktii <72131971+Arktii@users.noreply.github.com>
2025-06-13 20:33:56 +0800

    feat: setup font

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,9 +3,23 @@
 </script>
 
 <main class="bg-[url(src/lib/images/background/night-sky.png)] bg-repeat">
-	<p>Top of the screen</p>
+	<div class="flex flex-col items-center pt-10">
+		<div class="my-8 max-w-7/10">
+			<h1 class="font-lexend-giga text-4xl font-bold text-amber-50">Hi! I'm Emir.</h1>
+			<h2 class="font-lexend mb-10 text-2xl font-bold text-gray-400">
+				fresh grad, pls give job : >
+			</h2>
 
-	<Interactive />
+			<p class="text-amber-50">
+				Enjoy this interactive summary. Click around, catch some mice, then when you're done
+				exploring, navigate to the other details below.
+			</p>
 
-	<p>Bottom of the screen</p>
+			<p class="mt-10 text-center text-amber-50">Header Bar here</p>
+		</div>
+
+		<Interactive />
+
+		<p>Bottom of the screen</p>
+	</div>
 </main>

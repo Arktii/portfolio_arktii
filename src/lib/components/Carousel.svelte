f2e3da7e2e6db46ab2c784c018db5c57e33f542a
Arktii <72131971+Arktii@users.noreply.github.com>
2025-06-27 21:45:51 +0800

    feat: add carousel skipping navigation

--- src/lib/components/Carousel.svelte
+++ src/lib/components/Carousel.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
 	import { Icon } from 'svelte-icons-pack';
-	import { FiChevronLeft, FiChevronRight } from 'svelte-icons-pack/fi';
+	import { FiChevronLeft, FiChevronRight, FiCircle } from 'svelte-icons-pack/fi';
 
 	const buttonSize = 6;
 
@@ -23,6 +23,10 @@
 	function prev() {
 		current = (current - 1 + images.length) % images.length;
 	}
+
+	function select(index: number) {
+		current = index;
+	}
 </script>
 
 <div class="relative inline-block w-full {className}">
@@ -30,32 +34,46 @@
 		<img class="h-full w-full {pixelArt ? 'pixelated' : ''}" src={images[current]} {alt} />
 	{/if}
 	{#if images.length > 1}
-		<button
-			class="border-accent hover:border-secondary absolute bottom-2.5 left-2.5 z-11 cursor-pointer
-                items-center rounded-full border-1 transition-all duration-300 hover:scale-125"
-			onclick={prev}
-			aria-label="prev"
-		>
-			<Icon
-				src={FiChevronLeft}
-				color="var(--color-secondary)"
-				size="calc(var(--spacing) * {buttonSize})"
-			/>
-		</button>
-		<button
-			class="border-accent hover:border-secondary absolute right-2.5 bottom-2.5 z-11 cursor-pointer
-                items-center rounded-full border-1 transition-all duration-300 hover:scale-125"
-			onclick={next}
-			aria-label="next"
-		>
-			<Icon
-				src={FiChevronRight}
-				color="var(--color-secondary)"
-				size="calc(var(--spacing) * {buttonSize})"
-			/>
-		</button>
 		<div
-			class="bg-primary/50 pointer-events-none absolute right-0 bottom-0 left-0 z-10 h-12.5"
-		></div>
+			class="bg-primary/50 flex-between absolute right-0 bottom-0 left-0 flex h-12.5 flex-row items-center p-2"
+		>
+			<button
+				class="border-accent hover:border-secondary cursor-pointer
+                items-center rounded-full border-1 transition-all duration-300 hover:scale-125"
+				onclick={prev}
+				aria-label="prev"
+			>
+				<Icon
+					src={FiChevronLeft}
+					color="var(--color-secondary)"
+					size="calc(var(--spacing) * {buttonSize})"
+				/>
+			</button>
+			<div class="bottom-2 flex h-6 w-full flex-row items-center justify-center gap-1">
+				{#each { length: images.length } as _, i}
+					<button
+						class="cursor-pointer items-center rounded-full
+                                border-2 p-1.5 transition-all duration-300 hover:scale-135
+                                {i == current
+							? 'border-secondary bg-secondary'
+							: 'border-accent bg-transparent'}"
+						aria-label="image-{i}"
+						onclick={() => select(i)}
+					></button>
+				{/each}
+			</div>
+			<button
+				class="border-accent hover:border-secondary cursor-pointer
+                items-center rounded-full border-1 transition-all duration-300 hover:scale-125"
+				onclick={next}
+				aria-label="next"
+			>
+				<Icon
+					src={FiChevronRight}
+					color="var(--color-secondary)"
+					size="calc(var(--spacing) * {buttonSize})"
+				/>
+			</button>
+		</div>
 	{/if}
 </div>

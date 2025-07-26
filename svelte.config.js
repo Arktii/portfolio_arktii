a7aefd2d80b5b22ac30eac95681390cb7337348e
Arktii <72131971+Arktii@users.noreply.github.com>
2025-07-26 09:58:55 +0800

    feat: update config for vercel

--- svelte.config.js
+++ svelte.config.js
@@ -1,14 +1,19 @@
-import adapter from '@sveltejs/adapter-static';
+// import adapter from '@sveltejs/adapter-static';
+import vercel from '@sveltejs/adapter-vercel';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 const config = {
 	preprocess: vitePreprocess(),
 	kit: {
-		adapter: adapter({
-			fallback: '404.html'
-		}),
+		// adapter: adapter({
+		// 	fallback: '404.html'
+		// }),
+		// paths: {
+		// 	base: process.argv.includes('dev') ? '' : '/portfolio_arktii'
+		// }
+		adapter: vercel(),
 		paths: {
-			base: process.argv.includes('dev') ? '' : '/portfolio_arktii'
+			base: ''
 		}
 	}
 };
